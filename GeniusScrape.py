a5c0402e69984a546367dd07cd15bca43c0e4c15
dkeidar <dkeidar@users.noreply.github.com>
2020-02-05 14:13:34 -0500

    updated html, app, and scraper script

--- GeniusScrape.py
+++ GeniusScrape.py
@@ -5,30 +5,155 @@ Created on 1/5/20
 
 Note: Scrape Genius.com for artist lyrics
 """
+# Libraries for scraping Genius.com
 from bs4 import BeautifulSoup
 import requests
 import json
 import os
 import random
+# Spotify API python wrapper
+import spotipy
+import spotipy.oauth2 as oauth2
+import config
+# NLP Libraries
+from nltk import FreqDist
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.util import ngrams
+import re
+import pandas as pd
 
 
 def user_agent():
     directory = os.getcwd()
-    with open(directory + r'/user_agent.json', 'r') as file:
+    with open(directory + r"/user_agent.json", "r") as file:
         agent = json.load(file)
     return agent[random.randint(1, len(agent))]
 
 
-class GeniusScrape:
+header = user_agent()
 
-    def __init__(self, user_artist):
-        self.artist = user_artist
 
-    # removing characters to fit the formatting of Genius.com URL Structure
-    def format_artist(self):
-        artist_f = self.replace(' ', '-').replace('.', '').replace("'", '').replace('$', '-').lower()
-        return artist_f
+def spotify_auth():
+    client_id = config.sp_CLIENT_ID
+    client_secret = config.sp_CLIENT_SECRET
+    credentials = oauth2.SpotifyClientCredentials(client_id=client_id,
+                                                  client_secret=client_secret)
+    token = credentials.get_access_token()
+    sp = spotipy.Spotify(auth=token)
+    return sp
 
-    def url_gen(self):
-        geniusurl = 'https://wwww.genius.com/'
 
+# removing characters to fit the formatting of Genius.com URL Structure
+def format_name(user_artist):
+    punct = [(" ", "-"), (".", "-"), ("'", "-"), ("(", "-"), (")", "-version"), (",", "-")]
+    name = user_artist.lower()
+    for k, v in punct:
+        name = name.replace(k, v)
+        if "--" in name:
+            name = name.replace("--", "-")
+        if name.endswith("-"):
+            name = name[:-1]
+    return name
+
+
+# generate a list of albums by the artist using the spotify api
+def get_album_list(user_artist):
+    sp = spotify_auth()
+
+    artist_search = sp.search(q=f"{user_artist}", type="artist")
+    artist_id = artist_search["artists"]["items"][0]["id"]
+
+    album_list = []
+    album_search = sp.artist_albums(artist_id, album_type="album", limit=50)
+    for i in range(len(album_search["items"])):
+        album = album_search["items"][i]["name"]
+        if album not in album_list:
+            album_list.append(album)
+    return album_list
+
+
+def url_gen(artist_f, album_list):
+    geniusurl = "https://www.genius.com/albums/"
+    urllist = list()
+
+    mid_url = f'{geniusurl}{artist_f}'
+
+    for album in album_list:
+        album_f = format_name(album)
+        fin_url = f"{mid_url}/{album_f}"
+        album_url_dict = {"album_name": album,
+                          "url": fin_url}
+        urllist.append(album_url_dict)
+
+    return urllist
+
+
+def track_urls(album_url_dict):
+    updated_dict = []
+    for album in album_url_dict:
+        url = album["url"]
+        soup = BeautifulSoup(requests.get(url, headers=header).content, "lxml")
+        track_html = soup.find_all("div", {"class": "chart_row-content"})
+        for track in track_html:
+            track_url = track.a.get("href")
+            data = {"album_name": album["album_name"],
+                    "album_url": url,
+                    "track_url": track_url}
+            updated_dict.append(data)
+    return updated_dict
+
+
+def track_lyrics(track):
+    track_lyrics_list = []
+    song = BeautifulSoup(requests.get(track["track_url"], headers=header).content, "lxml")
+    lyricstext = song.find("div", {"class": "lyrics"})
+    try:
+        track_lyrics_list.append((track["album_name"],
+                                  track["album_url"],
+                                  song.h2.text.replace("\n", ""),
+                                  track["track_url"],
+                                  song.h1.text,
+                                  lyricstext.find("p").text.replace("\n", "")))
+    except ValueError:
+        track_lyrics_list.append((track["album_name"],
+                                  track["album_url"],
+                                  song.h2.text.replace("\n", ""),
+                                  track["track_url"],
+                                  song.h1.text,
+                                  "No Lyrics Available"))
+    return track_lyrics_list
+
+
+def clean_lyrics(track_lyrics_df):
+    tokenizer = RegexpTokenizer(r'\w+|\d+')
+    lemmatizer = WordNetLemmatizer()
+
+    cleaned = track_lyrics_df.lyrics.apply(lambda x: re.sub(r"\[(.*?)\]", ' ', x))
+    tokenized = cleaned.apply(lambda x: tokenizer.tokenize(x.lower().replace("'", '').replace('.', '')))
+    lemmatized = tokenized.apply(lambda x: [lemmatizer.lemmatize(word) for word in x])
+    no_stops = lemmatized.apply(lambda x: [word for word in x if word not in stopwords.words('english')])
+    return no_stops
+
+
+def word_freqs(track_lyrics_df, ngram='unigram', topwords=50):
+    word_list = []
+    for row in track_lyrics_df.lyrics_cleaned:
+        for word in row:
+            word_list.append(word)
+        word_list.append("songend")
+    if ngram == 'unigram':
+        counted = [str(word) for word in word_list if word != "songend"]
+    elif ngram == 'bigram':
+        counted = ngrams(word_list, 2)
+        counted = [str(word) for word in counted if "songend" not in word]
+    elif ngram == 'trigram':
+        counted = ngrams(word_list, 3)
+        counted = [str(word) for word in counted if "songend" not in word]
+    else:
+        print("try again")
+        pass
+    dist = FreqDist(counted).most_common(topwords)
+    distdf = pd.DataFrame(dist, columns=['term', 'count']).set_index('term')
+    return distdf
--- app.py
+++ app.py
@@ -10,7 +10,7 @@ Note: Flask app program
 from flask import Flask, render_template, request, redirect
 from flask_pymongo import PyMongo
 import sys
-from GeniusScrape import GeniusScrape as gs
+import GeniusScrape as gs
 
 # Initialize Flask Framework and Connect Mongo database
 app = Flask(__name__)
@@ -23,16 +23,20 @@ def home():
     return render_template("index.html", text=user_artist)
 
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['GET','POST'])
 def user_artist():
     artist = request.form['artist']
-    artist_f = gs.format_artist(artist)
-    return redirect(f"/?artist={artist_f}")
-
-
-def artist_scrape():
-    return gs.scrape(user_artist)
-
+    albums_urls = gs.track_urls(gs.url_gen(gs.format_name(artist), gs.get_album_list(artist)))
+    progress=0
+    track_lyrics_list = []
+    for index, track in enumerate(albums_urls):
+        progress += 1
+        track_lyrics_list = gs.track_lyrics(track)
+    return render_template('index.html',
+                           albums_urls=albums_urls,
+                           artist=artist,
+                           tracklyrics=track_lyrics_list,
+                           progress=progress)
 
 if __name__ == "__main__":
     app.run(debug=True)
